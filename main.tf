779bf359a758db8e201d8a0d551b1bba8a5a5214
Ho Kai Yip <ho.kai.yip@fullstackers.com>
2022-11-17 11:16:29 -0500

    test 6

--- main.tf
+++ main.tf
@@ -100,6 +100,6 @@ resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id = azurerm_linux_web_app.webapp.id
   repo_url = "https://github.com/hokaiyipFS/php-docs-hello-world"
   branch = "main"
-  use_manual_integration = true
+  use_manual_integration = false
   use_mercurial = false
 }
