8736afba72da880c9f00f7f0bd759a1cdfbcdd17
Ho Kai Yip <ho.kai.yip@fullstackers.com>
2022-11-22 13:44:41 -0500

    use_manual_integration = true

--- main.tf
+++ main.tf
@@ -106,6 +106,6 @@ resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id = azurerm_linux_web_app.webapp.id
   repo_url = "https://dev.azure.com/hokaiyip/_git/Terraform-Testing2"
   branch = "main"
-  use_manual_integration = false
+  use_manual_integration = true
   use_mercurial = false
 }
